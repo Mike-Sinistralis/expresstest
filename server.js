2e04252fea3a894a3a2cdf32913166c421d98354
Michael Zimmerman <mzimmerman@turningtechnologies.com>
2015-10-21 16:53:24 -0400

    Page 78 of 352. Playing mostly with middleware

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 var express = require('express'),
+	compression = require('compression'),
+	path = require('path'),
+	config = require('./serverConfig'),
+	logger = require('morgan'),
+	cookieParser = require('cookieParser'),
+	bodyParser = require('body-parser'),
 	app = express();
 
+var controllers = {
+	index: require('./controllers/index')
+}
 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('x-powered-by', false);
+app.set('view engine', 'ejs' );
+
+
+app.use(compression({threshold: 1}));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use('/', controllers.index);
+
+app.listen(config.build.port, function() {
+	console.log('The server is running on port %s', config.build.port);
+});
